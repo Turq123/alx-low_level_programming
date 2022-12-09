7daf59c4cb486f3e717b5d1d12a727738020a5bc
Turquoise <quayedanturquoise@gmail.com>
2022-12-09 23:53:43 +0000

    Programming is like building a multilingual puzzle..

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 /**
  * This file prints
  * Programming is like building a multilingual puzzl
- * using puts
+ * using put
+ * Return: 0
  */
 #include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
-	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
