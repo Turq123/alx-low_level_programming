26b918f6bdd1ac5fe581b102ad544856c38763c6
Turquoise <quayedanturquoise@gmail.com>
2022-12-08 20:27:35 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <studio.h>
+/**
+ * * main - puts coding method
+ * * 
+ * * Return : 0
+ * */
+
+int main(viod)
+{
+	            puts("Programming is like building a multilingual puzzle");
+		    return;
+}
