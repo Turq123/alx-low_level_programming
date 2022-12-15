1b2a368b9dbb7721561793507d6804fef992add7
Turquoise <quayedanturquoise@gmail.com>
2022-12-15 05:49:28 +0000

    h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "task.h"
+
 
 /**
  * main - Entry point
@@ -10,8 +11,15 @@
 
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	printf("_putchar\n");
-
-	return  (0);
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/task.h
@@ -0,0 +1,7 @@
+#ifndef TASK_H_
+#define TASK_H_
+#include <stdio.h>
+
+int _putchar(char c);
+
+#endif
